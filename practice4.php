9eca03f471f0dc8b31b8ca4cf14134fe717966df
Ryland Edwards <rylandcedwards@gmail.com>
2023-10-30 19:51:17 -0500

    Update practice4.php

--- practice4.php
+++ practice4.php
@@ -1,11 +1,19 @@
-$num = 5; // replace 5 with your input number
+?php
+$num = isset($_POST['size']) ? intval($_POST['size']) : 1; // replace 1 with your default number
 
 echo "<table border='1'>";
+echo "<tr><th></th>";
+for ($i = 1; $i <= $num; $i++) {
+    echo "<th>" . $i . "</th>";
+}
+echo "</tr>";
 for ($i = 1; $i <= $num; $i++) {
     echo "<tr>";
+    echo "<th>" . $i . "</th>";
     for ($j = 1; $j <= $num; $j++) {
         echo "<td>" . $i * $j . "</td>";
     }
     echo "</tr>";
 }
 echo "</table>";
+?>
